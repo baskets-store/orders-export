a8b1575b436ee51633f7114e7d7c740aa7b08bc1
lauryngrootfaam <grootfaamlauryn@gmail.com>
2022-10-20 13:20:01 +0200

    added the ordername to the demo version

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 var script = document.createElement('script');script.src = "https://unpkg.com/object-exporter@3.2.1/dist/objectexporter.min.js";document.getElementsByTagName('head')[0].appendChild(script);
     
 const checked = document.querySelectorAll(".-checked");
+let ordername = document.querySelectorAll('.-checked .-primary a')
 
 const ordersId = [];
 let order =[];
+let random = [];
 const array = [];
 const exportData = [];
 
@@ -16,11 +18,16 @@ async function f () {
     
      await fetch(`https://freshcotton-343466.webshopapp.com/admin/orders/${ordersId[i]}/products/edit_locations.json`).then(data => data.json()).then(data => order.push(data.orderProducts));
 };
-console.log(ordersId + order)
+
+for (let i = 0; i <ordername.length; i++){
+  random.push({"ordername": ordername[i].innerText});
+}
 
 await order.forEach(order => {
-    order.forEach(item =>  array.push({"ordernummer": item.order_id, "date": item.created_at, "productbrand": item.order_product.brand_name, "product-title": item.variant.product.nl.title,  "productbarcode": item.order_product.barcode, "qnty": item.order_product.quantity ,"sku": item.order_product.sku,"location": item.location_name }))
+    order.forEach(item =>  array.push({"ordername": document.querySelector(`tr[data-id="${item.order_id}"]`).querySelector('a').innerHTML.trim()
+    ,"orderid": item.order_id, "date": item.created_at, "productbrand": item.order_product.brand_name, "product-title": item.variant.product.nl.title,  "productbarcode": item.order_product.barcode, "qnty": item.order_product.quantity ,"sku": item.order_product.sku,"location": item.location_name }))
 });
+
 
 for (let i = 0; i< array.length; i++){
     if (array[i].location == activeStore) {
@@ -31,12 +38,9 @@ await objectExporter({
     exportable: exportData,
     type: 'csv',
     fileName: 'export',
-    headers: ["order number", "date", "product brand", "product name", "product barcode", "qnty", "sku", "location"],
+    headers: ["order", "order id", "date", "product brand", "product name", "product barcode", "qnty", "sku", "location"],
   })
 
 }
 
 f();
-
-
-
