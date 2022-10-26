08f88d6d6bc678c42168ba6c2c0ca49360e116f8
lauryngrootfaam <grootfaamlauryn@gmail.com>
2022-10-26 09:13:40 +0200

    added the necessary information and removed the unnecessary information

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var script = document.createElement('script');script.src = "https://unpkg.com/object-exporter@3.2.1/dist/objectexporter.min.js";document.getElementsByTagName('head')[0].appendChild(script);
     
 const checked = document.querySelectorAll(".-checked");
-let ordername = document.querySelectorAll('.-checked .-primary a')
+let ordername = document.querySelectorAll('.-checked .-primary a');
 
 const ordersId = [];
 let order =[];
@@ -17,15 +17,16 @@ async function f () {
     ordersId.push(checked[i].getAttribute('data-id'));
     
      await fetch(`https://freshcotton-343466.webshopapp.com/admin/orders/${ordersId[i]}/products/edit_locations.json`).then(data => data.json()).then(data => order.push(data.orderProducts));
+    
 };
 
 for (let i = 0; i <ordername.length; i++){
   random.push({"ordername": ordername[i].innerText});
 }
 
+
 await order.forEach(order => {
-    order.forEach(item =>  array.push({"ordername": document.querySelector(`tr[data-id="${item.order_id}"]`).querySelector('a').innerHTML.trim()
-    ,"orderid": item.order_id, "date": item.created_at , "productbrand": item.order_product.brand_name, "product-title": item.variant.product.nl.title,  "productbarcode": item.order_product.barcode, "qnty": item.order_product.quantity ,"sku": item.order_product.sku,"location": item.location_name }))
+    order.forEach(item =>  array.push({"ordername": document.querySelector(`tr[data-id="${item.order_id}"]`).querySelector('a').innerHTML.trim(), "date": item.created_at, "product-title": item.variant.product.nl.title, "qnty": item.order_product.quantity, "sku": item.order_product.sku ,"size": item.order_product.variant_title, "location": item.location_name, "status": document.querySelector(`tr[data-id="${item.order_id}"]`).querySelector('.LabelNoWrap').innerHTML.trim() }))
 });
 
 
@@ -36,13 +37,17 @@ for (let i = 0; i< array.length; i++){
 
 await objectExporter({
     exportable: exportData,
-    type: 'csv',
+    type: 'xls',
     fileName: 'export',
-    headers: ["order", "order id", "date", "product brand", "product name", "product barcode", "qnty", "sku", "location"],
+    headers: ["Order", "Datum Order", "Item", "Qnty", "SKU (Itemcode)", "Size", "Location", "Status"],
   })
 
 }
 
 f();
 
+// Date.UTC(year, monthIndex, day, hour, minute, second, millisecond)
+// year = item.order[i].created_at.substr(0, 4);
+// maand = item.order[i].created_at.substr(5, 2);
+// dag =item.order[i].created_at.substr(8, 2);
 
