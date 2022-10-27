e2342306a8e1b13df905b2b91c99a29f62cb97fb
lauryngrootfaam <grootfaamlauryn@gmail.com>
2022-10-27 09:09:15 +0200

    latest version

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
 //baskets demo versie
 
-var script = document.createElement('script');script.src = "https://unpkg.com/object-exporter@3.2.1/dist/objectexporter.min.js";document.getElementsByTagName('head')[0].appendChild(script);
-const checked = document.querySelectorAll(".-checked");
-let ordername = document.querySelectorAll('.-checked .-primary a');
-const ordersId = [];
-let order =[];
-let random = [];
-const array = [];
-const exportData = [];
-const activeStore  = 'Jordaan Fictief';
-async function f () {
-    for (var i = 0; i < checked.length; i++){
-    ordersId.push(checked[i].getAttribute('data-id'));
-     await fetch(`https://freshcotton-343466.webshopapp.com/admin/orders/${ordersId[i]}/products/edit_locations.json`).then(data => data.json()).then(data => order.push(data.orderProducts));
-};
-for (let i = 0; i <ordername.length; i++){
-  random.push({"ordername": ordername[i].innerText});
-}
-await order.forEach(order => {
-    order.forEach(item =>  array.push({"ordername": document.querySelector(`tr[data-id="${item.order_id}"]`).querySelector('a').innerHTML.trim(), "date": item.created_at, "product-title": item.variant.product.nl.title, "qnty": item.order_product.quantity, "sku": item.order_product.sku ,"size": item.order_product.variant_title, "location": item.location_name, "status": document.querySelector(`tr[data-id="${item.order_id}"]`).querySelector('.LabelNoWrap').innerHTML.trim() }))
-});
-for (let i = 0; i< array.length; i++){
-    if (array[i].location == activeStore) {
-       await exportData.push(array[i])
-}}
-await objectExporter({
-    exportable: exportData,
-    type: 'xls',
-    fileName: 'export',
-    headers: ["Order", "Datum Order", "Item", "Qnty", "SKU (Itemcode)", "Size", "Location", "Status"],
-  })
-}
-f();
+// var script = document.createElement('script');script.src = "https://unpkg.com/object-exporter@3.2.1/dist/objectexporter.min.js";document.getElementsByTagName('head')[0].appendChild(script);
+// const checked = document.querySelectorAll(".-checked");
+// let ordername = document.querySelectorAll('.-checked .-primary a');
+// const ordersId = [];
+// let order =[];
+// let random = [];
+// const array = [];
+// const exportData = [];
+// const activeStore  = 'Jordaan Fictief';
+// async function f () {
+//     for (var i = 0; i < checked.length; i++){
+//     ordersId.push(checked[i].getAttribute('data-id'));
+//      await fetch(`https://freshcotton-343466.webshopapp.com/admin/orders/${ordersId[i]}/products/edit_locations.json`).then(data => data.json()).then(data => order.push(data.orderProducts));
+// };
+// for (let i = 0; i <ordername.length; i++){
+//   random.push({"ordername": ordername[i].innerText});
+// }
+// await order.forEach(order => {
+//     order.forEach(item =>  array.push({"ordername": document.querySelector(`tr[data-id="${item.order_id}"]`).querySelector('a').innerHTML.trim(), "date": item.created_at, "product-title": item.variant.product.nl.title, "qnty": item.order_product.quantity, "sku": item.order_product.sku ,"size": item.order_product.variant_title, "location": item.location_name, "status": document.querySelector(`tr[data-id="${item.order_id}"]`).querySelector('.LabelNoWrap').innerHTML.trim() }))
+// });
+// for (let i = 0; i< array.length; i++){
+//     if (array[i].location == activeStore) {
+//        await exportData.push(array[i])
+// }}
+// await objectExporter({
+//     exportable: exportData,
+//     type: 'xls',
+//     fileName: 'export',
+//     headers: ["Order", "Datum Order", "Item", "Qnty", "SKU (Itemcode)", "Size", "Location", "Status"],
+//   })
+// }
+// f();
 
 
 // URL voor demo versie (jordaan)
--- index.js
+++ index.js
@@ -1,163 +1,148 @@
 //alle code
 //de code: voor alle locaties
 // var script = document.createElement('script');script.src = "https://unpkg.com/object-exporter@3.2.1/dist/objectexporter.min.js";document.getElementsByTagName('head')[0].appendChild(script);
-    
 // const checked = document.querySelectorAll(".-checked");
-
+// let ordername = document.querySelectorAll('.-checked .-primary a');
 // const ordersId = [];
 // let order =[];
+// let random = [];
 // const array = [];
 // const exportData = [];
-
-// const activeStore  = 'Jordaan Fictief';
-
+/// const activeStore  = '';
 // async function f () {
-//     for (var i = 0; i < checked.length; i++){ 
+//     for (var i = 0; i < checked.length; i++){
 //     ordersId.push(checked[i].getAttribute('data-id'));
-    
-//      await fetch(`https://freshcotton-343466.webshopapp.com/admin/orders/${ordersId[i]}/products/edit_locations.json`).then(data => data.json()).then(data => order.push(data.orderProducts));
+//      await fetch(`https://baskets.webshopapp.com/admin/orders/${ordersId[i]}/products/edit_locations.json`).then(data => data.json()).then(data => order.push(data.orderProducts));
 // };
-// console.log(ordersId + order)
-
+// for (let i = 0; i <ordername.length; i++){
+//   random.push({"ordername": ordername[i].innerText});
+// }
 // await order.forEach(order => {
-//     order.forEach(item =>  array.push({"ordernummer": item.order_id, "date": item.created_at, "productbrand": item.order_product.brand_name, "product-title": item.variant.product.nl.title,  "productbarcode": item.order_product.barcode, "qnty": item.order_product.quantity ,"sku": item.order_product.sku,"location": item.location_name }))
+//     order.forEach(item =>  array.push({"ordername": document.querySelector(`tr[data-id="${item.order_id}"]`).querySelector('a').innerHTML.trim(), "date": item.created_at, "product-title": item.variant.product.nl.title, "qnty": item.order_product.quantity, "sku": item.order_product.sku ,"size": item.order_product.variant_title, "location": item.location_name, "status": document.querySelector(`tr[data-id="${item.order_id}"]`).querySelector('.LabelNoWrap').innerHTML.trim() }))
 // });
-
-
-
+// for (let i = 0; i< array.length; i++){
+// 
+//        await exportData.push(array[i])
+//}
 
 // await objectExporter({
-//     exportable: array,
-//     type: 'csv',
+//     exportable: exportData,
+//     type: 'xls',
 //     fileName: 'export',
-//     headers: ["order number", "date", "product brand", "product name", "product barcode", "qnty", "sku", "location"],
+//     headers: ["Order", "Datum Order", "Item", "Qnty", "SKU (Itemcode)", "Size", "Location", "Status"],
 //   })
-
 // }
-
 // f();
+
+
+
+
 
 
 // code voor de Jordaan
 // var script = document.createElement('script');script.src = "https://unpkg.com/object-exporter@3.2.1/dist/objectexporter.min.js";document.getElementsByTagName('head')[0].appendChild(script);
-    
 // const checked = document.querySelectorAll(".-checked");
-
+// let ordername = document.querySelectorAll('.-checked .-primary a');
 // const ordersId = [];
 // let order =[];
+// let random = [];
 // const array = [];
 // const exportData = [];
-
 // const activeStore  = 'Baskèts Jordaan';
-
 // async function f () {
-//     for (var i = 0; i < checked.length; i++){ 
+//     for (var i = 0; i < checked.length; i++){
 //     ordersId.push(checked[i].getAttribute('data-id'));
-    
 //      await fetch(`https://baskets.webshopapp.com/admin/orders/${ordersId[i]}/products/edit_locations.json`).then(data => data.json()).then(data => order.push(data.orderProducts));
 // };
-// console.log(ordersId + order)
-
+// for (let i = 0; i <ordername.length; i++){
+//   random.push({"ordername": ordername[i].innerText});
+// }
 // await order.forEach(order => {
-//     order.forEach(item =>  array.push({"ordernummer": item.order_id, "date": item.created_at, "productbrand": item.order_product.brand_name, "product-title": item.variant.product.nl.title,  "productbarcode": item.order_product.barcode, "qnty": item.order_product.quantity ,"sku": item.order_product.sku,"location": item.location_name }))
+//     order.forEach(item =>  array.push({"ordername": document.querySelector(`tr[data-id="${item.order_id}"]`).querySelector('a').innerHTML.trim(), "date": item.created_at, "product-title": item.variant.product.nl.title, "qnty": item.order_product.quantity, "sku": item.order_product.sku ,"size": item.order_product.variant_title, "location": item.location_name, "status": document.querySelector(`tr[data-id="${item.order_id}"]`).querySelector('.LabelNoWrap').innerHTML.trim() }))
 // });
-
 // for (let i = 0; i< array.length; i++){
 //     if (array[i].location == activeStore) {
 //        await exportData.push(array[i])
 // }}
-
 // await objectExporter({
 //     exportable: exportData,
-//     type: 'csv',
+//     type: 'xls',
 //     fileName: 'export',
-//     headers: ["order number", "date", "product brand", "product name", "product barcode", "qnty", "sku", "location"],
+//     headers: ["Order", "Datum Order", "Item", "Qnty", "SKU (Itemcode)", "Size", "Location", "Status"],
 //   })
-
 // }
-
 // f();
+
+
 
 
 // Code voor de Pijp
 // var script = document.createElement('script');script.src = "https://unpkg.com/object-exporter@3.2.1/dist/objectexporter.min.js";document.getElementsByTagName('head')[0].appendChild(script);
-    
 // const checked = document.querySelectorAll(".-checked");
-
+// let ordername = document.querySelectorAll('.-checked .-primary a');
 // const ordersId = [];
 // let order =[];
+// let random = [];
 // const array = [];
 // const exportData = [];
-
 // const activeStore  = 'Baskèts Pijp';
-
 // async function f () {
-//     for (var i = 0; i < checked.length; i++){ 
+//     for (var i = 0; i < checked.length; i++){
 //     ordersId.push(checked[i].getAttribute('data-id'));
-    
 //      await fetch(`https://baskets.webshopapp.com/admin/orders/${ordersId[i]}/products/edit_locations.json`).then(data => data.json()).then(data => order.push(data.orderProducts));
 // };
-// console.log(ordersId + order)
-
+// for (let i = 0; i <ordername.length; i++){
+//   random.push({"ordername": ordername[i].innerText});
+// }
 // await order.forEach(order => {
-//     order.forEach(item =>  array.push({"ordernummer": item.order_id, "date": item.created_at, "productbrand": item.order_product.brand_name, "product-title": item.variant.product.nl.title,  "productbarcode": item.order_product.barcode, "qnty": item.order_product.quantity ,"sku": item.order_product.sku,"location": item.location_name }))
+//     order.forEach(item =>  array.push({"ordername": document.querySelector(`tr[data-id="${item.order_id}"]`).querySelector('a').innerHTML.trim(), "date": item.created_at, "product-title": item.variant.product.nl.title, "qnty": item.order_product.quantity, "sku": item.order_product.sku ,"size": item.order_product.variant_title, "location": item.location_name, "status": document.querySelector(`tr[data-id="${item.order_id}"]`).querySelector('.LabelNoWrap').innerHTML.trim() }))
 // });
-
 // for (let i = 0; i< array.length; i++){
 //     if (array[i].location == activeStore) {
 //        await exportData.push(array[i])
 // }}
-
 // await objectExporter({
 //     exportable: exportData,
-//     type: 'csv',
+//     type: 'xls',
 //     fileName: 'export',
-//     headers: ["order number", "date", "product brand", "product name", "product barcode", "qnty", "sku", "location"],
+//     headers: ["Order", "Datum Order", "Item", "Qnty", "SKU (Itemcode)", "Size", "Location", "Status"],
 //   })
-
 // }
-
 // f();
+
+
 
 
 //freshcotton / baskets kantoor
-// var script = document.createElement('script');script.src = "https://unpkg.com/object-exporter@3.2.1/dist/objectexporter.min.js";document.getElementsByTagName('head')[0].appendChild(script);
-    
-// const checked = document.querySelectorAll(".-checked");
-
-// const ordersId = [];
-// let order =[];
-// const array = [];
-// const exportData = [];
-
-// const activeStore  = 'Freshcotton';
-
-// async function f () {
-//     for (var i = 0; i < checked.length; i++){ 
-//     ordersId.push(checked[i].getAttribute('data-id'));
-    
-//      await fetch(`https://baskets.webshopapp.com/admin/orders/${ordersId[i]}/products/edit_locations.json`).then(data => data.json()).then(data => order.push(data.orderProducts));
-// };
-// console.log(ordersId + order)
-
-// await order.forEach(order => {
-//     order.forEach(item =>  array.push({"ordernummer": item.order_id, "date": item.created_at, "productbrand": item.order_product.brand_name, "product-title": item.variant.product.nl.title,  "productbarcode": item.order_product.barcode, "qnty": item.order_product.quantity ,"sku": item.order_product.sku,"location": item.location_name }))
-// });
-
-// for (let i = 0; i< array.length; i++){
-//     if (array[i].location == activeStore) {
-//        await exportData.push(array[i])
-// }}
-
-// await objectExporter({
-//     exportable: exportData,
-//     type: 'csv',
-//     fileName: 'export',
-//     headers: ["order number", "date", "product brand", "product name", "product barcode", "qnty", "sku", "location"],
-//   })
-
-// }
-
-// f();
-
-
+var script = document.createElement('script');script.src = "https://unpkg.com/object-exporter@3.2.1/dist/objectexporter.min.js";document.getElementsByTagName('head')[0].appendChild(script);
+const checked = document.querySelectorAll(".-checked");
+let ordername = document.querySelectorAll('.-checked .-primary a');
+const ordersId = [];
+let order =[];
+let random = [];
+const array = [];
+const exportData = [];
+const activeStore  = 'Freshcotton';
+async function f () {
+    for (var i = 0; i < checked.length; i++){
+    ordersId.push(checked[i].getAttribute('data-id'));
+     await fetch(`https://baskets.webshopapp.com/admin/orders/${ordersId[i]}/products/edit_locations.json`).then(data => data.json()).then(data => order.push(data.orderProducts));
+};
+for (let i = 0; i <ordername.length; i++){
+  random.push({"ordername": ordername[i].innerText});
+}
+await order.forEach(order => {
+    order.forEach(item =>  array.push({"ordername": document.querySelector(`tr[data-id="${item.order_id}"]`).querySelector('a').innerHTML.trim(), "date": item.created_at, "product-title": item.variant.product.nl.title, "qnty": item.order_product.quantity, "sku": item.order_product.sku ,"size": item.order_product.variant_title, "location": item.location_name, "status": document.querySelector(`tr[data-id="${item.order_id}"]`).querySelector('.LabelNoWrap').innerHTML.trim() }))
+});
+for (let i = 0; i< array.length; i++){
+    if (array[i].location == activeStore) {
+       await exportData.push(array[i])
+}}
+await objectExporter({
+    exportable: exportData,
+    type: 'xls',
+    fileName: 'export',
+    headers: ["Order", "Datum Order", "Item", "Qnty", "SKU (Itemcode)", "Size", "Location", "Status"],
+  })
+}
+f();
