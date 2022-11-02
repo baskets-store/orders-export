0cfe751381d1acf55568d5f58a8f54b48b8261fa
lauryngrootfaam <grootfaamlauryn@gmail.com>
2022-11-02 16:54:36 +0100

    latest update

--- index.js
+++ index.js
@@ -1,81 +1,82 @@
 //alle code
 // de code: voor alle locaties
-
-
-// var script = document.createElement('script');script.src = "https://unpkg.com/object-exporter@3.2.1/dist/objectexporter.min.js";document.getElementsByTagName('head')[0].appendChild(script);
-// const checked = document.querySelectorAll(".-checked");
-// let ordername = document.querySelectorAll('.-checked .-primary a');
-// const ordersId = [];
-// let order =[];
-// const array = [];
-// let orders =[];
-// const exportData = [];
-// // const activeStore  = 'Kantoor';
+var script = document.createElement('script');script.src = "https://unpkg.com/object-exporter@3.2.1/dist/objectexporter.min.js";document.getElementsByTagName('head')[0].appendChild(script);
+const checked = document.querySelectorAll(".-checked");
+let ordername = document.querySelectorAll('.-checked .-primary a');
+const ordersId = [];
+let order =[];
+const array = [];
+let orders =[];
+const exportData = [];
+const activeStore = "";
 
 
 
-// async function f () {
-//     for (var i = 0; i < checked.length; i++){
-//     ordersId.push(checked[i].getAttribute('data-id'));
-//      await fetch(`https://baskets.webshopapp.com/admin/orders/${ordersId[i]}.json`).then(data => data.json()).then(data => orders.push(data.order));
-// };
+async function f () {
+    for (var i = 0; i < checked.length; i++){
+    ordersId.push(checked[i].getAttribute('data-id'));
+     await fetch(`https://baskets.webshopapp.com/admin/orders/${ordersId[i]}.json`).then(data => data.json()).then(data => orders.push(data.order));
+};
 
 
-//  await orders.forEach(order =>{
+ await orders.forEach(order =>{
 
-//     order.order_products.forEach(product => {
+    order.order_products.forEach(product => {
 
-//     const lineItem = {
-//         ordername: order.number,
-//         date: order.location_order_products.find(item => item.order_product_id === product.id)?.created_at,
-//         name: product.product_title,
-//         qnty: product.quantity,
-//         sku: product.sku,
-//         size: product.variant_title,
-//         location: order.location_order_products.find(item => item.order_product_id === product.id)?.location_name,
-//         status: order.location_order_products.find(item => item.order_product_id === product.id)?.stock_status   
-//     };
-//     array.push(lineItem);    
-//     })
-// });
+    const lineItem = {
+        ordername: order.number,
+        date: order.location_order_products.find(item => item.order_product_id === product.id)?.created_at || order.created_at,
+        name: product.product_title,
+        qnty: product.quantity,
+        sku: product.sku,
+        size: product.variant_title,
+        location: order.location_order_products.find(item => item.order_product_id === product.id)?.location_name || order.inventory_location,
+        status: order.location_order_products.find(item => item.order_product_id === product.id)?.stock_status   ||  order.status
+    };
+    array.push(lineItem);    
+    })
+});
+ 
+
+for (let i = 0; i< array.length; i++){
+
+       await exportData.push(array[i])
+};
+
+const date = new Date();
+
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+let hours = date.getHours();
+let minutes = date.getMinutes();
 
 
-// for (let i = 0; i< array.length; i++){
 
-//        await exportData.push(array[i])
-// };
+if (activeStore == "Baskèts Jordaan"){
+    fileName = `jordaan_export_${day}_${month}_${year}_${hours}:${minutes}`;
+}
+else if (activeStore == "Baskèts De Pijp"){
+    fileName = `de_pijp_export_${day}_${month}_${year}_${hours}:${minutes}`;
+}
+else if (activeStore == "Kantoor"){
+    fileName = `kantoor_export_${day}_${month}_${year}_${hours}:${minutes}`;
+}
+else if (activeStore == "") {
+    fileName = `order_export_allelocaties_${day}_${month}_${year}_${hours}:${minutes}`;
+};
 
-// const date = new Date();
+await objectExporter({
+    exportable: exportData,
+    type: 'xls',
+    fileName: fileName,
+    headers: ["Order", "Datum Order", "Item", "Qnty", "SKU (Itemcode)", "Size", "Location", "Status"],
+  })
+}
+f();
 
-// let day = date.getDate();
-// let month = date.getMonth() + 1;
-// let year = date.getFullYear();
-// let hours = date.getHours();
-// let minutes = date.getMinutes();
 
-// let fileName = "";
 
-// if (activeStore == "Baskèts Jordaan"){
-//     fileName = `jordaan_export_${day}_${month}_${year}_${hours}:${minutes}`;
-// }
-// else if (activeStore == "Baskèts De Pijp"){
-//     fileName = `de_pijp_export_${day}_${month}_${year}_${hours}:${minutes}`;
-// }
-// else if (activeStore == "Kantoor"){
-//     fileName = `kantoor_export_${day}_${month}_${year}_${hours}:${minutes}`;
-// }
-// else {
-//     fileName = `order_export_allelocaties_${day}_${month}_${year}_${hours}:${minutes}`;
-// };
-
-// await objectExporter({
-//     exportable: exportData,
-//     type: 'xls',
-//     fileName: fileName,
-//     headers: ["Order", "Datum Order", "Item", "Qnty", "SKU (Itemcode)", "Size", "Location", "Status"],
-//   })
-// }
-// f();
 
 
 
